879f577951bc306bc548c913df863dd5096d16c7
NishantSatapara-Sigmoid <98634598+NishantSatapara-Sigmoid@users.noreply.github.com>
2022-03-16 18:12:15 +0530

    Add files via upload

---
+++ sql_files/create_table.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE IF NOT EXISTS weather(
+        STATE VARCHAR(30),
+        DESCRIPTION varchar(30),
+        TEMPERATURE decimal,
+        FEELS_LIKE_TEMPERATURE decimal,
+        MIN_TEMP decimal,
+        MAX_TEMP decimal,
+        HUMIDITY numeric,
+        CLOUDS numeric)
---
+++ sql_files/insert_into_table.sql
@@ -0,0 +1,1 @@
+COPY weather FROM '/store_files_mysql/Weather_Data.csv' DELIMITER ',' CSV HEADER
